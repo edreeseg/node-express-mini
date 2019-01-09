5f09cb37c02ca99b355df1e1d2f224850a5ea186
edreeseg <edward.reeseg@gmail.com>
2019-01-09 13:03:38 -0500

    Basic get and post requests to api/users

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // implement your API here
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const db = require('./data/db');
+
+const server = express();
+server.use(bodyParser.json());
+
+const STATUS_USER_ERROR = 422;
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json({ users });
+        })
+        .catch(err => {
+            res.status(500);
+            res.json({ error: "The users information could not be retrieved." });
+        });
+});
+
+server.post('/api/users', (req, res) => {
+    const [ name, bio ] = [ req.body.name, req.body.bio ];
+    if (!name || !bio){
+        res.status(400);
+        res.json({ errorMessage: 'Please provide name and bio for the user.' });
+        return;
+    }
+    db.insert({ name, bio })
+        .then(obj => {
+            db.findById(obj.id)
+                .then(user => {
+                    res.status(201);
+                    res.json(user);
+                })
+                .catch(err => {
+                    res.status(500);
+                    res.json({ error: "There was an error while saving the user to the database" });
+                });
+        })
+        .catch(err => {
+            res.status(500);
+            res.json({ error: "There was an error while saving the user to the database" });
+        });
+});
+
+server.listen(5000);
